02797be6e73cf99e72c2cf1dcbf076986858ae38
Robin Reicher <robin.reicher@xyleminc.com>
2020-02-24 09:10:55 +0100

    Ugly start

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,33 +1,23 @@
 import World from '../world.js'
-import IsoPlugin from 'phaser3-plugin-isometric';
 
 export default class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
             key: 'GameScene',
-	    mapAdd: { isoPlugin: 'iso' }
         });
     }
 
     preload() {
 	console.log("GameScene - preload");
-	this.load.scenePlugin({
-	    key: 'IsoPlugin',
-	    url: IsoPlugin,
-	    sceneKey: 'iso'
-	});
     }
     create() {
 	console.log("GameScene - create");
-	this.isoGroup = this.add.group();
-
-	this.iso.projector.origin.setTo(0.5, 0.3);
 
 	// Add some tiles to our scene
 	this.world = new World(this, 10)
 	let start = this.world.coordToPixel(9, 9, 1)
-	console.log(start)
-	this.player = this.add.isoSprite(start[0], start[1], start[2], 'player', this.isoGroup);
+	//console.log(start)
+	//this.player = this.add.isoSprite(start[0], start[1], start[2], 'spritesheet', 'player-0', this.isoGroup);
     }
 
     update(time, delta) {
--- src/world.js
+++ src/world.js
@@ -3,27 +3,40 @@ export default class World {
 	this.group = scene.add.group()
 	this.grid = new Array(size)
 	this.tileSize = 38
+	this.scene = scene
 
-	for (var x = 0; x < this.grid.length; x++) {
+	this.create(size)
+    }
+    create(size){
+	for (var x = 0; x < size; x++)
+	{
 	    this.grid[x] = new Array(size)
-	    for (var y = 0; y < this.grid[x].length; y++) {
-		var tile = scene.add.isoSprite(x*this.tileSize,
-					       y*this.tileSize,
-					       0,
-					       'tile',
-					       this.group)
+            for (var y = 0; y < size; y++)
+            {
+		var tx = x * this.tileSize + (this.tileSize/2)
+		var ty = y * this.tileSize + (this.tileSize/2)
+		var tile = this.scene.add.image(tx,
+					  ty,
+					  'sprites',
+					  'tile',
+					  this.group);
+
+		tile.setData('row', x);
+		tile.setData('col', y);
+		tile.setDepth(ty);
+
 		tile.setInteractive();
 		tile.on('pointerover', function() {
 		    this.setTint(0x888888);
-		    this.isoZ += 5;
+		    //this.isoZ += 5;
 		});
 
 		tile.on('pointerout', function() {
 		    this.clearTint();
-		    this.isoZ -= 5;
+		    //this.isoZ -= 5;
 		});
 		this.grid[x][y] = tile
-	    }
+            }
 	}
     }
     coordToPixel( x, y, z ){
